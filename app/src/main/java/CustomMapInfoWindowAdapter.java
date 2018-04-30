5c633766d697425023279a66ce63ca2af1c49430
Frida Sjögren <frisjh112@studentmail.oru.se>
2018-04-30 14:21:00 +0200

    Markörer anpassas

---
+++ app/src/main/java/CustomMapInfoWindowAdapter.java
@@ -0,0 +1,56 @@
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+import org.w3c.dom.Text;
+
+import sup.savemeaspot.R;
+
+/**
+ * Adapter för att hantera anpassade informationsrutor i Google Maps
+ * Created by Frida on 2018-04-30.
+ */
+
+public class CustomMapInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private Activity context;
+
+    public CustomMapInfoWindowAdapter(Activity context){
+        this.context = context;
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    public View getInfoWindowContents(Marker marker, String locality, String category, String description) {
+        View view = context.getLayoutInflater().inflate(R.layout.custom_info_window_marker, null);
+
+        //Hämtar alla textview från layout
+        TextView spotTitleTV = view.findViewById(R.id.info_window_title);
+        TextView spotCategoryTV = view.findViewById(R.id.info_window_category);
+        TextView spotDescriptionTV = view.findViewById(R.id.info_window_description);
+        TextView localityTV = view.findViewById(R.id.info_window_real_location);
+        TextView coordinatesTV = view.findViewById(R.id.info_window_coordinates);
+
+        spotTitleTV.setText(marker.getTitle());
+        spotDescriptionTV.setText(description);
+        spotCategoryTV.setText(category);
+        localityTV.setText(locality);
+        coordinatesTV.setText(marker.getPosition().latitude + ", " + marker.getPosition().longitude);
+
+        return view;
+    }
+}
